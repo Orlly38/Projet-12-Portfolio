96d75e1e0845c1b28773a328feb2a96bec6cfa3d
Aurélie <dorche.aurelie@gmail.com>
2024-01-14 14:30:55 +0100

    #7

--- script/skills_generic.js
+++ script/skills_generic.js
@@ -1,13 +1,16 @@
 function addSkills(skillsData,title){
+    //Récupération container compétences
     let container = document.querySelector("#skills .all-skills");
+    //Création div pour la compétence donnée, son titre et la div des images
     let skill = document.createElement("div");
     let skillImgs = document.createElement("div");
     let titleHtml = document.createElement("h3");
     titleHtml.appendChild(document.createTextNode(title));
     skill.appendChild(titleHtml);
     skillsData.forEach(skillData => {
+        //Pour chaque image du tableau skillsData on crée une div avec l'image dedans et on ajoute
         let skillsDiv = document.createElement("div");   
-        skillsDiv.innerHTML += '<img src="./images/'+skillData.image+'" />';
+        skillsDiv.innerHTML += '<img src="./images/'+skillData.image+'" title="'+skillData.text+'"/>';
         skillsDiv.classList.add("skill-img");
         skillImgs.appendChild(skillsDiv);
     });
--- script/skills_test.js
+++ script/skills_test.js
@@ -1,14 +1,25 @@
 function addSkillsTest(){
     const skillsData = [
         {
-            "image":"cypress.jpeg"
+            "image":"cypress.jpeg",
+            "text":"text cypress"
         },
         {
-            "image":"cdr.png"
+            "image":"cdr.png",
+            "text":"text cypress"
         },
         {
-            "image":"js.jpg"
-        }
+            "image":"testexplo.png",
+            "text":"text cypress"
+        },
+        {
+            "image":"bilan.png",
+            "text":"text cypress"
+        },
+        {
+            "image":"revueexi.png",
+            "text":"text cypress"
+        },
     ];
     addSkills(skillsData, 'Test');
 }
